8fc9d968101e3939b930ef997fd6d7fa7ee36364
matytyma <matytyma22@gmail.com>
2025-05-11 20:25:24 +0200

    feat(db): Create teachers schema

---
+++ backend/src/main/kotlin/schema/Teachers.kt
@@ -0,0 +1,14 @@
+package dev.matytyma.schema
+
+import org.jetbrains.exposed.sql.Table
+
+object Teachers : Table("teacher") {
+    val id = integer("id").autoIncrement()
+    val firstName = varchar("first_name", 50)
+    val lastName = varchar("last_name", 50)
+    val shortName = varchar("short_name", 5)
+    val prefixTitle = varchar("prefix_title", 50)
+    val suffixTitle = varchar("suffix_title", 50)
+
+    override val primaryKey = PrimaryKey(id)
+}
