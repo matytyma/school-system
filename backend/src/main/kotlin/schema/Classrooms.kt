336a5442b151f8dcbbb4fc032b5320656d10b85e
matytyma <matytyma22@gmail.com>
2025-05-11 21:01:09 +0200

    feat(db): Create classroom schema

---
+++ backend/src/main/kotlin/schema/Classrooms.kt
@@ -0,0 +1,12 @@
+package dev.matytyma.schema
+
+import org.jetbrains.exposed.sql.Table
+
+object Classrooms : Table("classroom") {
+    val id = integer("id").autoIncrement()
+    val name = varchar("name", 50)
+    val room = reference("room", Rooms.id)
+    val teacher = reference("teacher", Teachers.id)
+
+    override val primaryKey = PrimaryKey(id)
+}
