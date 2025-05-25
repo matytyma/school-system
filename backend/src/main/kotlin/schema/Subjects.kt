5b250a79038a65aa0773e9136753691faef4c76a
matytyma <matytyma22@gmail.com>
2025-05-25 14:33:26 +0200

    feat(db): Create subjects schema

---
+++ backend/src/main/kotlin/schema/Subjects.kt
@@ -0,0 +1,11 @@
+package dev.matytyma.schema
+
+import org.jetbrains.exposed.sql.Table
+
+object Subjects : Table("subject") {
+    val id = integer("id").autoIncrement()
+    val name = varchar("name", 50)
+    val shortName = varchar("short_name", 4)
+
+    override val primaryKey = PrimaryKey(id)
+}
