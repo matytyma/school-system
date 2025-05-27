46a7baf578844213555327e4816e1ca31a322d70
matytyma <matytyma22@gmail.com>
2025-05-27 21:24:19 +0200

    feat(db): Make teacher titles nullable

--- backend/src/main/kotlin/schema/Teachers.kt
+++ backend/src/main/kotlin/schema/Teachers.kt
@@ -7,8 +7,8 @@ object Teachers : Table("teacher") {
     val firstName = varchar("first_name", 50)
     val lastName = varchar("last_name", 50)
     val shortName = varchar("short_name", 5)
-    val prefixTitle = varchar("prefix_title", 50)
-    val suffixTitle = varchar("suffix_title", 50)
+    val prefixTitle = varchar("prefix_title", 50).nullable()
+    val suffixTitle = varchar("suffix_title", 50).nullable()
 
     override val primaryKey = PrimaryKey(id)
 }
