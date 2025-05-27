4edc8160feb0b8ba02ab863ba21f89d599cda662
matytyma <matytyma22@gmail.com>
2025-05-27 21:34:46 +0200

    feat(db): Create lessons schema

---
+++ backend/src/main/kotlin/schema/Lessons.kt
@@ -0,0 +1,23 @@
+package dev.matytyma.schema
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.kotlin.datetime.date
+import org.jetbrains.exposed.sql.or
+
+object Lessons : Table("lesson") {
+    val id = integer("id").autoIncrement()
+    val classroom = reference("classroom", Classrooms.id).nullable()
+    val group = reference("group", Classrooms.id).nullable()
+    val date = date("date")
+    val index = short("index")
+    val subject = reference("subject", Subjects.id)
+    val teacher = reference("teacher", Teachers.id)
+    val topic = varchar("topic", 100).nullable()
+
+    override val primaryKey = PrimaryKey(id)
+
+    init {
+        uniqueIndex(classroom, group, date, index)
+        check { (classroom neq null) or (group neq null) }
+    }
+}
