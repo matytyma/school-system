b372aeb9a27db0ab97625ba8630bde70af699a04
matytyma <matytyma22@gmail.com>
2025-06-08 20:14:55 +0200

    feat(fe): Display sample teachers data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div
       className="flex items-center justify-center h-screen font-bold font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-5xl bold">School Management System</h1>
+        <Link href="/teachers">See our teachers</Link>
       </div>
     </div>
   );
---
+++ frontend/src/app/teachers/page.tsx
@@ -0,0 +1,27 @@
+export default async function Teachers() {
+  console.log(await fetch("http://localhost:3000/api/teacher/list").then(it => it.text()));
+  const teachers: {
+    id: number,
+    firstName: string,
+    lastName: string,
+    prefixTitle: string | undefined,
+    suffixTitle: string | undefined,
+  }[] = await fetch("http://localhost:3000/api/teacher/list", {cache: "no-store"})
+    .then(it => it.json());
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>Identifier</th>
+        <th>Full Name</th>
+      </tr>
+      </thead>
+      <tbody className="border-2">
+      {teachers.map(({id, firstName, lastName, prefixTitle, suffixTitle}) => <tr key={id}>
+        <td>{id}</td>
+        <td>{prefixTitle && `${prefixTitle} `}{firstName} {lastName}{suffixTitle && ` ${suffixTitle}`}</td>
+      </tr>)}
+      </tbody>
+    </table>
+  )
+}
