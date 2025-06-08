58072cde47a9e8dcc5baa7df7448e3c50fee7d90
matytyma <matytyma22@gmail.com>
2025-06-08 19:27:08 +0200

    feat(fe): Cleanup homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,10 @@
 export default function Home() {
   return (
-    <>
-      <div
-        className="flex items-center justify-center h-screen font-bold font-[family-name:var(--font-geist-sans)]">
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="text-5xl bold">School Management System</h1>
-          <div>Hello</div>
-        </div>
+    <div
+      className="flex items-center justify-center h-screen font-bold font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-5xl bold">School Management System</h1>
       </div>
-    </>
+    </div>
   );
 }
